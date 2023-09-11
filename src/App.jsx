cf0df9a815f016b8489bf96baf3431cabb9865bb
Md Saifur Rahman <saifurrshishir@gmail.com>
2023-09-11 19:13:21 +0600

    fake data added with json file

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
-
-import './App.css'
+import "./App.css";
+import Blogs from "./components/Header/Blogs/Blogs";
+import Header from "./components/Header/Header";
 
 function App() {
   return (
     <>
-    <h1 className='text-6xl bg-blue-600'>Knowledge Cafe</h1>     
-       
+      <Header></Header>
+      <Blogs></Blogs>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/Header/Blogs/Blogs.jsx
@@ -0,0 +1,18 @@
+import { useState } from "react";
+
+const Blogs = () => {
+    const [blogs, setBlogs] =  useState([]);
+
+    useState(()=>{
+        fetch("blogs.json")
+        .then(res => res.json())
+        .then(data => setBlogs(data))
+    }, [])
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default Blogs;
